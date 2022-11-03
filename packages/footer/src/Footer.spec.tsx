da81f9ead79ec3d0f39b194c4c2fe3fac204ee7c
Santosh Gudikandula <santosh.gudikandula@zalando.de>
2022-11-03 14:27:08 +0100

    test version 9

--- packages/footer/src/Footer.spec.tsx
+++ packages/footer/src/Footer.spec.tsx
@@ -2,11 +2,11 @@
  * @jest-environment jsdom
  */
 
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Footer } from './Footer';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Footer } from "./Footer";
 
-test('renders footer', () => {
+test("renders footer cmp", () => {
   const { getByText } = render(<Footer />);
   const linkElement = getByText(/footer/i);
   expect(linkElement).toBeDefined();
