dc674ee71400212f56b37811e8f2d05bc6bdc536
Santosh Gudikandula <santosh.gudikandula@zalando.de>
2022-11-03 10:23:10 +0100

    Validating nx

--- packages/remixapp/app/root.tsx
+++ packages/remixapp/app/root.tsx
@@ -25,7 +25,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
+        <LiveReload /> 
       </body>
     </html>
   );
